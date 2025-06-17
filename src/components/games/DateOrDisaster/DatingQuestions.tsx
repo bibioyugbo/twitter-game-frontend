5293191660d3f7ce0119a16ff914a3c43a2d00ae
bibi oyugbo <obehioyeoyugbo@gmail.com>
2025-06-17 10:02:15 +0100

    Adjusting responsiveness2

--- src/components/games/DateOrDisaster/DatingQuestions.tsx
+++ src/components/games/DateOrDisaster/DatingQuestions.tsx
@@ -98,18 +98,18 @@ export default function DatingQuestions(){
             <DateOrDisasterWrapper>
                 {loading?
                     <Loader showLoader={loading}/>:
-                    <div className="h-dvh overflow-scroll items-center justify-center flex flex-col gap-4">
+                    <div className=" overflow-scroll items-center justify-center flex flex-col gap-4">
                         <div className="text-white font-[Satoshi-Bold] text-base md:text-xl">
                             {"0" + questionCount} of 08
                         </div>
                         <Progress style={{marginTop:"5px", marginBottom:"5px"}} value={progress} className="h-[21px]"/>
-                        <div className="bg-[#EBEBEB4D] w-full md:w-full md:max-w-[508px] px-2 py-2 flex items-center justify-center rounded-[40px] border-2 border-[#DEE4FF2E]   ">
+                        <div className="bg-[#EBEBEB4D] w-full md:w-full md:max-w-[508px] px-2 py-2 md:px-4 md:py-4 flex items-center justify-center rounded-[40px] border-2 border-[#DEE4FF2E]   ">
                             <div className="bg-white px-5 py-5 md:max-w-[476px]  rounded-[32px]">
                                 {currentQuestions2?.map((item:Question,questionIndex)=>{
                                     console.log(currentQuestions2)
                                     return(
                                         <div key={questionIndex}>
-                                            <div className={"text-[#003A58] font-[Satoshi-Bold] text-base md:text-xl mt-4"}>
+                                            <div className={"text-[#003A58] font-[Satoshi-Bold] text-base md:text-xl mt-4 md:mt-6"}>
                                                 {item.question}
                                             </div>
                                             <div className={"text-[#646363] font-[Satoshi-Bold] cursor-pointer mt-9 flex flex-col gap-2 text-sm md:text-base "}>
@@ -131,7 +131,7 @@ export default function DatingQuestions(){
                                 })}
                             </div>
                         </div>
-                        <DefaultButton shake={isNotSelected} customClass={`w-full max-w-[232px] md:max-w-[444px] bottom-[calc(20px+env(safe-area-inset-bottom))]`}  style={{ marginBottom:"10px" ,height:"55px", padding:0}} onClick={()=>goToNextQuestion()} text={questionCount===8?"What Dater are you?":"Continue"} />
+                        <DefaultButton shake={isNotSelected} customClass={`w-full h-[55px] md:h-[65px] max-w-[232px] md:mt-4 md:max-w-[444px] bottom-[calc(20px+env(safe-area-inset-bottom))]`}  style={{padding:0}} onClick={()=>goToNextQuestion()} text={questionCount===8?"What Dater are you?":"Continue"} />
                     </div>
                 }
             </DateOrDisasterWrapper>
--- src/components/layouts/DateOrDisasterWrapper.tsx
+++ src/components/layouts/DateOrDisasterWrapper.tsx
@@ -13,7 +13,7 @@ export default function DateOrDisasterWrapper({...props}:DateOrDisasterWrapperPr
             {/*<div className={"bg-gradient-to-br h-screen from-yellow-200 via-yellow-400 to-yellow-600"}>*/}
             {/*    {children}*/}
             {/*</div>*/}
-            <div style={props.style} className={'no-border app-wrapper date-or-disaster p-5 flex items-center flex-col justify-center w-full h-dvh'}>
+            <div style={props.style} className={'no-border app-wrapper date-or-disaster p-5 flex items-center flex-col justify-center w-full '}>
                 <div className={"no-border flex-1 flex items-center h-dvh justify-center"}>
                     <div className={'no-border'}>
                         {props.children}
