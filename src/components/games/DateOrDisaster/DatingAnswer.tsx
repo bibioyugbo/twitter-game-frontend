6804fc6ac2e59d95d5bbfa3018385be126a9e851
bibi oyugbo <obehioyeoyugbo@gmail.com>
2025-08-04 17:58:32 +0100

    Added socials

--- src/components/games/DateOrDisaster/DatingAnswer.tsx
+++ src/components/games/DateOrDisaster/DatingAnswer.tsx
@@ -451,8 +451,7 @@ export default function DatingAnswer (){
                         </div>
 
 
-                    <footer className={"no-border footer-text flex-nowrap text-xs md:text-sm text-white"}>Made with ❤️ by <span className={"no-border underline"}>Adaeze</span> and <span className={" no-border underline"}>Bibi</span></footer>
-
+                    <footer className={"no-border footer-text flex-nowrap text-xs md:text-sm text-white"}>Made with ❤️ by <a className={"no-border underline"} target="_blank" rel="noopener noreferrer"  href="https://x.com/adathedesigner">Adaeze</a> and <a className={"no-border underline"} target="_blank" rel="noopener noreferrer"  href="https://x.com/bibibuilds">Bibi</a></footer>
 
                 </div>
 
